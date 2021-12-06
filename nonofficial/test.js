65792ebd1c0888923c36ea3a1974502e9347ad90
Conic Games <64161905+conicgames@users.noreply.github.com>
2021-12-06 08:12:30 -0500

    Update test.js

--- nonofficial/test.js
+++ nonofficial/test.js
@@ -8,7 +8,7 @@ var id = "nonofficial_differential_calculus"
 var name = "Differential Calculus*";
 var description = "A implementation of the 'Differential Calculus' theory from the game.";
 var authors = "Gilles-Philippe Paillé";
-var version = 1;
+var version = 2;
 
 var currency;
 var q1 = BigNumber.ZERO, q2 = BigNumber.ONE, q3 = BigNumber.ONE, q4 = BigNumber.ONE;
