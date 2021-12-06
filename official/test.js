037f9f16ccb13c6fd9e8420ee74b4a82778c8bf0
Conic Games <64161905+conicgames@users.noreply.github.com>
2021-12-06 11:40:32 -0500

    Update test.js

--- official/test.js
+++ official/test.js
@@ -204,7 +204,7 @@ var getTertiaryEquation = () => Localization.format(stringTickspeed, getTickspee
 
 var getPublicationMultiplier = (tau) => tau.pow(0.164) / BigNumber.THREE;
 var getPublicationMultiplierFormula = (symbol) => "\\frac{{" + symbol + "}^{0.164}}{3}";
-var getTau = () => ((currency.value + BigNumber.One).Log10() + BigNumber.One).Log10();
+var getTau = () => ((currency.value + BigNumber.One).log10() + BigNumber.One).log10();
 var get2DGraphValue = () => currency.value.sign * (BigNumber.ONE + currency.value.abs()).log10().toNumber();
 
 var postPublish = () => {
