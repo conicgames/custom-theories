aacd056563bac9ae71d0fc363cb77e6a7585f506
sudo-ellipsis <57691445+sudo-ellipsis@users.noreply.github.com>
2021-12-08 00:27:33 +0800

    frongl bongl
    testing something funny and cool i guess

--- official/test.js
+++ official/test.js
@@ -5,7 +5,7 @@ import { theory } from "../api/Theory";
 import { Utils } from "../api/Utils";
 
 var id = "recurrence_relation";
-var name = "Recurrence Relation*";
+var name = "frongl bongl*";
 var description = "A implementation of the 'Recurrence Relation' theory from the game.";
 var authors = "Gilles-Philippe Paillé";
 var version = 3;
