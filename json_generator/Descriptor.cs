ed7507e3403fdf83c00c048c0a75b4972d2d27d7
conicgames <conic.games.studio@gmail.com>
2021-12-09 00:32:34 -0500

    Added json_generator

---
+++ json_generator/Descriptor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UpdateJson
+{
+    public class Descriptor
+    {
+        public string RelativeUrl { get; set; } = null;
+        public string Id { get; set; } = null;
+        public string Name { get; set; } = null;
+        public string Description { get; set; } = null;
+        public string Authors { get; set; } = null;
+        public string Version { get; set; } = "1";
+    }
+}
---
+++ json_generator/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace UpdateJson
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var folders = new List<string>() { "official", "nonofficial" };
+            var descriptors = new List<Descriptor>();
+            bool hasError = false;
+
+            foreach (var folder in folders)
+            {
+                foreach (string file in Directory.EnumerateFiles(folder, "*.js", SearchOption.AllDirectories))
+                {
+                    var script = File.ReadAllText(file);
+
+                    try
+                    {
+                        var descriptor = Parser.Parse(script);
+                        descriptor.RelativeUrl = file.Replace('\\', '/');
+                        descriptors.Add(descriptor);
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine("Could not parse '" + file + "': " + e.Message);
+                        hasError = true;
+                    }
+                }
+            }
+
+            if (hasError)
+            {
+                Console.WriteLine("One or more error occured. The json file was not generated.");
+                return -1;
+            }
+
+            bool isFirst = true;
+            var json = new StringBuilder();
+
+            json.Append("[\n");
+
+            foreach (var descriptor in descriptors)
+            {
+                if (!isFirst)
+                {
+                    json.Append(",\n");
+                }
+                else
+                {
+                    isFirst = false;
+                }
+
+                json.Append("  {\n");
+                json.Append("    \"relative_url\": \"" + Escape(descriptor.RelativeUrl) + "\",\n");
+                json.Append("    \"id\": \"" + Escape(descriptor.Id) + "\",\n");
+                json.Append("    \"name\": \"" + Escape(descriptor.Name) + "\",\n");
+                json.Append("    \"description\": \"" + Escape(descriptor.Description) + "\",\n");
+                json.Append("    \"authors\": \"" + Escape(descriptor.Authors) + "\",\n");
+                json.Append("    \"version\": \"" + Escape(descriptor.Version) + "\",\n");
+                json.Append("  }");
+            }
+
+            json.Append("\n]");
+
+            File.WriteAllText("theories.json", json.ToString());
+
+            return 0;
+        }
+
+        private static string Escape(string text)
+        {
+            return text.Replace("\\", "\\\\")
+                       .Replace("\"", "\\\"")
+                       .Replace("\n", "\\n")
+                       .Replace("\r", "\\r")
+                       .Replace("\t", "\\t");
+        }
+    }
+}
