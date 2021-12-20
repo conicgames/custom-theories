006c9232438dfaf2a8204393aafef0a14ae77a64
xelaroc <91007922+xelaroc@users.noreply.github.com>
2021-12-20 14:55:36 +0100

    added milestones & rebalanced

--- nonofficial/CT-WeierstrassSineProduct.js
+++ nonofficial/CT-WeierstrassSineProduct.js
@@ -4,9 +4,9 @@ import { parseBigNumber, BigNumber } from "./api/BigNumber";
 import { theory } from "./api/Theory";
 import { Utils } from "./api/Utils";
 
-var id = "weierstrass-product-sine";
+var id = "weierstrass-product-sine_test";
 var name = "Weierstraß Sine Product";
-var description = "Exploit the inaccuracy of sine's product representation, a result due to Euler which was rigorously proved later by Weierstraß using his famous Factorization Theorem.\n\nIntuitively, the idea behind this formula is to factorize sine using its roots (sine has zeros at each multiple of π), just as one would do for a polynomial.\n\nThe product s_n represents the n first factors of this infinite product (together with the root at x=0), which means there is some error between s_n(x) and the actual sin(x), depending on n and x. Note that the truncated product s_n approximates sin(x) better for bigger n and smaller x, in particular the approximation becomes bad for a fixed n when x gets large in the sense that the ratio s_n(x)/sin(x) diverges for x -> infty.\n\nHere, the derivative of q with respect to time is set to s_n(χ)/sin(χ) i.e. the ratio from before evaluated at χ (chi), which itself is a value depending on n. Note that increasing n both increases χ and the accuracy of the approximation s_n.";
+var description = "Exploit the inaccuracy of sine's product representation, a result due to Euler which was rigorously proved later by Weierstraß using his famous Factorization Theorem.\n\nIntuitively, the idea behind this formula is to factorize sine using its roots (sine has zeros at each multiple of π), just as one would do for a polynomial.\n\nThe product s_n represents only the n first factors of this infinite product (together with the root at x=0), which means there is some error between s_n(x) and the actual sin(x), depending on n and x. Note that this truncated product s_n approximates sin(x) better for bigger n and smaller x, in particular the approximation becomes bad for a fixed n when x gets large in the sense that the ratio s_n(x)/sin(x) diverges for x -> infty.\n\nHere, the derivative of q with respect to time is set to s_n(χ)/sin(χ) i.e. the ratio from before evaluated at χ (chi), which itself is a value depending on n. Note that increasing n both increases χ and the accuracy of the approximation s_n.";
 var authors = "xelaroc (AlexCord#6768)";
 var version = 1;
 
@@ -32,7 +32,7 @@ var init = () => {
     {
         let getDesc = (level) => "q_1=" + getQ1(level).toString(0);
         let getInfo = (level) => "q_1=" + getQ1(level).toString(0);
-        q1 = theory.createUpgrade(0, currency, new FirstFreeCost(new ExponentialCost(10, 3.36843/4)));
+        q1 = theory.createUpgrade(0, currency, new FirstFreeCost(new ExponentialCost(10, 3.38/4)));
         q1.getDescription = (amount) => Utils.getMath(getDesc(q1.level));
         q1.getInfo = (amount) => Utils.getMathTo(getInfo(q1.level), getInfo(q1.level + amount));
     }
@@ -41,7 +41,7 @@ var init = () => {
     {
         let getDesc = (level) => "q_2=2^{" + level + "}";
         let getInfo = (level) => "q_2=" + getQ2(level).toString(0);
-        q2 = theory.createUpgrade(1, currency, new ExponentialCost(1000, 3.36843*3));
+        q2 = theory.createUpgrade(1, currency, new ExponentialCost(1000, 3.38*3));
         q2.getDescription = (amount) => Utils.getMath(getDesc(q2.level));
         q2.getInfo = (amount) => Utils.getMathTo(getInfo(q2.level), getInfo(q2.level + amount));
     }
@@ -50,7 +50,7 @@ var init = () => {
     {
         let getDesc = (level) => "n=" + level;
         let getInfo = (level) => "n=" + level;
-        n = theory.createUpgrade(2, currency, new ExponentialCost(20, 3.36843));
+        n = theory.createUpgrade(2, currency, new ExponentialCost(20, 3.38));
         n.getDescription = (amount) => Utils.getMath(getDesc(n.level));
         n.getInfo = (amount) => Utils.getMathTo(getInfo(n.level), getInfo(n.level + amount));
         n.bought = (_) => updateSineRatio_flag = true;
@@ -60,7 +60,7 @@ var init = () => {
     {
         let getDesc = (level) => "c_1=" + getC1(level).toString(0);
         let getInfo = (level) => "c_1=" + getC1(level).toString(0);
-        c1 = theory.createUpgrade(3, currency, new ExponentialCost(50, 3.36843/1.5));
+        c1 = theory.createUpgrade(3, currency, new ExponentialCost(50, 3.38/1.5));
         c1.getDescription = (amount) => Utils.getMath(getDesc(c1.level));
         c1.getInfo = (amount) => Utils.getMathTo(getInfo(c1.level), getInfo(c1.level + amount));
         c1.bought = (_) => updateSineRatio_flag = true;
@@ -70,7 +70,7 @@ var init = () => {
     {
         let getDesc = (level) => "c_2=2^{" + level + "}";
         let getInfo = (level) => "c_2=" + getC2(level).toString(0);
-        c2 = theory.createUpgrade(4, currency, new ExponentialCost(1e10, 3.36843*10));
+        c2 = theory.createUpgrade(4, currency, new ExponentialCost(1e10, 3.38*10));
         c2.getDescription = (amount) => Utils.getMath(getDesc(c2.level));
         c2.getInfo = (amount) => Utils.getMathTo(getInfo(c2.level), getInfo(c2.level + amount));
     }
@@ -83,10 +83,10 @@ var init = () => {
 
     /////////////////////
     // Checkpoint Upgrades
-    theory.setMilestoneCost(new CustomCost(lvl => BigNumber.from(lvl < 5 ? 1 + lvl : 10)));
+    theory.setMilestoneCost(new CustomCost(lvl => BigNumber.from(lvl < 5 ? 1 + 1.5*lvl : lvl < 6 ? 10 : lvl < 7 ? 14 : 20)));
 
     {
-        q1Exp = theory.createMilestoneUpgrade(0, 2);
+        q1Exp = theory.createMilestoneUpgrade(0, 4);
         q1Exp.description = Localization.getUpgradeIncCustomExpDesc("q_1", "0.01");
         q1Exp.info = Localization.getUpgradeIncCustomExpInfo("q_1", "0.01");
         q1Exp.boughtOrRefunded = (_) => theory.invalidatePrimaryEquation();
@@ -126,7 +126,7 @@ var tick = (elapsedTime, multiplier) => {
         let vn = getN(n.level);
         let vc1 = getC1(c1.level);
         chi = BigNumber.PI * vc1 * vn / (vc1 + vn / BigNumber.THREE.pow(BigNumber.from(chiDivN.level))) + BigNumber.ONE;
-        S = Utils.getWeierstrassSineProd(n.level,chi.toNumber())/chi.sin();
+        S = Utils.getWeierstrassSineProd(n.level,chi)/chi.sin();
         updateSineRatio_flag = false;
     }
     let dq = dt * S * vc2;
@@ -155,8 +155,7 @@ var getPrimaryEquation = () => {
     theory.primaryEquationHeight = 120;
     let result = "\\begin{matrix} s_n(x) := x\\cdot\\displaystyle\\prod_{k=1}^n\\left(1-\\frac{x}{k\\pi}^{\\ 2}\\right) \\\\"
     result += "\\dot{\\rho}=q_1";
-    if (q1Exp.level == 1) result += "^{1.01}";
-    if (q1Exp.level == 2) result += "^{1.02}";
+    if (q1Exp.level > 0) result += `^{${1+q1Exp.level*0.01}}`;
     result += "q_2q,\\quad\\dot{q} =\\displaystyle\\frac{s_n(\\chi)}{\\sin(\\chi)}";
     if (c2Term.level > 0) result += "\\cdot c_2";
     result += "\\end{matrix}"
